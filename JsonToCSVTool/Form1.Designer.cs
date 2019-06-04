2487ac38f3114843afe8efcacbbeeedd91c1b7c1
张鹏 <scbeta@qq.com>
2019-06-04 11:38:05 +0800

    First commit.

---
+++ JsonToCSVTool/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace JsonToCSVTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvMain = new System.Windows.Forms.DataGridView();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnPaste = new System.Windows.Forms.Button();
+            this.btnSaveToCSVFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMain
+            // 
+            this.dgvMain.AllowUserToAddRows = false;
+            this.dgvMain.AllowUserToDeleteRows = false;
+            this.dgvMain.AllowUserToOrderColumns = true;
+            this.dgvMain.AllowUserToResizeRows = false;
+            this.dgvMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMain.Location = new System.Drawing.Point(12, 41);
+            this.dgvMain.Name = "dgvMain";
+            this.dgvMain.RowTemplate.Height = 23;
+            this.dgvMain.Size = new System.Drawing.Size(706, 450);
+            this.dgvMain.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(160, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "打开JSON文件...";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.BtnOpen_Click);
+            // 
+            // btnPaste
+            // 
+            this.btnPaste.Location = new System.Drawing.Point(178, 12);
+            this.btnPaste.Name = "btnPaste";
+            this.btnPaste.Size = new System.Drawing.Size(160, 23);
+            this.btnPaste.TabIndex = 2;
+            this.btnPaste.Text = "从剪贴板粘贴";
+            this.btnPaste.UseVisualStyleBackColor = true;
+            this.btnPaste.Click += new System.EventHandler(this.BtnPaste_Click);
+            // 
+            // btnSaveToCSVFile
+            // 
+            this.btnSaveToCSVFile.Enabled = false;
+            this.btnSaveToCSVFile.Location = new System.Drawing.Point(344, 12);
+            this.btnSaveToCSVFile.Name = "btnSaveToCSVFile";
+            this.btnSaveToCSVFile.Size = new System.Drawing.Size(160, 23);
+            this.btnSaveToCSVFile.TabIndex = 3;
+            this.btnSaveToCSVFile.Text = "保存为CSV文件...";
+            this.btnSaveToCSVFile.UseVisualStyleBackColor = true;
+            this.btnSaveToCSVFile.Click += new System.EventHandler(this.BtnSaveToCSVFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 503);
+            this.Controls.Add(this.btnSaveToCSVFile);
+            this.Controls.Add(this.btnPaste);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.dgvMain);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JSON转CSV工具";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvMain;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnPaste;
+        private System.Windows.Forms.Button btnSaveToCSVFile;
+    }
+}
+
---
+++ JsonToCSVTool/Form1.cs
@@ -0,0 +1,143 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JsonToCSVTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "全部文件(*.txt;*.json)|*.txt;*.json";
+            var ret = ofd.ShowDialog();
+            if (ret == DialogResult.Cancel)
+                return;
+            var file = ofd.FileName;
+            var content = File.ReadAllText(file, Encoding.Default);
+            load(content);
+        }
+
+        private void BtnPaste_Click(object sender, EventArgs e)
+        {
+            var content = Clipboard.GetText();
+            if (string.IsNullOrEmpty(content))
+            {
+                MessageBox.Show("剪贴板中没有文字内容。");
+                return;
+            }
+            load(content);
+        }
+
+        private JArray findArray(JObject obj)
+        {
+            foreach (var token in obj.PropertyValues())
+            {
+                if (token is JArray)
+                    return (JArray)token;
+                else if (token is JObject)
+                {
+                    var tmpArray = findArray((JObject)token);
+                    if (tmpArray != null)
+                        return tmpArray;
+                }
+            }
+            return null;
+        }
+
+        private void load(string json)
+        {
+            JArray items = null;
+            try
+            {
+                var token = JToken.Parse(json);
+                if (token is JArray)
+                    items = (JArray)token;
+                else if (token is JObject)
+                {
+                    items = findArray((JObject)token);
+                }
+                if (items == null)
+                    throw new ApplicationException("items is null");
+            }
+            catch
+            {
+                MessageBox.Show("导入文本不是一个有效的JSON格式，或者JSON对象中没有包括数组！");
+                return;
+            }
+            try
+            {                
+                DataTable dt = new DataTable();
+                bool columnAdded = false;
+                foreach (JObject item in items)
+                {
+                    //添加列
+                    if (!columnAdded)
+                    {
+                        columnAdded = true;
+                        foreach (var property in item.Properties())
+                        {
+                            dt.Columns.Add(property.Name);
+                        }
+                    }
+                    List<object> valueList = new List<object>();
+                    foreach (JValue value in item.PropertyValues())
+                        valueList.Add(value.Value);
+
+                    dt.Rows.Add(valueList.ToArray());
+                }
+                dgvMain.DataSource = dt;
+                btnSaveToCSVFile.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void BtnSaveToCSVFile_Click(object sender, EventArgs e)
+        {
+            var dt = dgvMain.DataSource as DataTable;
+            if (dt == null)
+            {
+                MessageBox.Show("当前没有数据！");
+                return;
+            }
+            var sfd = new SaveFileDialog();
+            sfd.Filter = "CSV文件(*.csv)|*.csv";
+            var ret = sfd.ShowDialog();
+            if (ret == DialogResult.Cancel)
+                return;
+            using (var stream = File.OpenWrite(sfd.FileName))
+            using (var writer = new StreamWriter(stream, Encoding.Default))
+            {
+                List<string> list = new List<string>();
+                foreach (DataColumn column in dt.Columns)
+                {
+                    list.Add(column.ColumnName);
+                }
+                writer.WriteLine(string.Join(",", list.ToArray()));
+                foreach (DataRow row in dt.Rows)
+                {
+                    list.Clear();
+                    foreach (var item in row.ItemArray)
+                        list.Add(item.ToString());
+                    writer.WriteLine(string.Join(",", list.ToArray()));
+                }
+                writer.Close();
+            }
+            MessageBox.Show("保存成功！");
+        }
+    }
+}
